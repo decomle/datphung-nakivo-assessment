e5198044923de7a8a514196a95813c2cf1577c09
Dat Phung <thedat230589@gmail.com>
2026-08-27 08:44:18 +0700

    remove redundant code

--- assessment/src/main/java/com/nakivo/assessment/job/repository/JobRepository.java
+++ assessment/src/main/java/com/nakivo/assessment/job/repository/JobRepository.java
@@ -2,11 +2,9 @@ package com.nakivo.assessment.job.repository;
 
 import com.nakivo.assessment.job.entity.Job;
 import com.nakivo.assessment.job.entity.JobStatus;
-import jakarta.persistence.LockModeType;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Lock;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 
